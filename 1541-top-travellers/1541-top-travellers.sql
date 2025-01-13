115fa0aac93d7b520cbf8309faa14fea6f79bcc2
Tanmay Sharma <tanmay.rsh@gmail.com>
2025-01-12 21:55:54 -0500

    Time: 1463 ms (29.12%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -2,7 +2,7 @@
 -- select U.name as name, SUM(CASE WHEN R.distance IS NOT NULL THEN R.distance ELSE 0 END) as travelled_distance
 -- from Users U LEFT JOIN Rides R 
 -- ON U.id = R.user_id
--- GROUP BY U.name
+-- GROUP BY U.id,U.name
 -- ORDER BY travelled_distance DESC, U.name ASC;
 
 select U.name, COALESCE(SUM(R.distance),0) as travelled_distance
