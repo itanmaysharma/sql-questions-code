7d6a7423c5e7180c1d3d1e3878ff47d8e2005d64
Tanmay Sharma <tanmay.rsh@gmail.com>
2025-01-15 20:47:08 -0500

    Time: 733 ms (19.99%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select * 
 From Patients
-Where conditions REGEXP '(^|\\s)DIAB1';
+-- Where conditions REGEXP '(^|\\s)DIAB1';
+where conditions like "DIAB1%" or conditions like "% DIAB1%";
